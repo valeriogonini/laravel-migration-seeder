a7861de0afd7127d76f884ce560a1f41eaf975ee
valeriogonini <valeriogonini@gmail.com>
2024-05-27 15:46:22 +0200

    add db

--- database/migrations/2024_05_27_132600_create_trains_table.php
+++ database/migrations/2024_05_27_132600_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('departure_station', 50);
+            $table->string('company', 50);
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('numeber_wagons');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
